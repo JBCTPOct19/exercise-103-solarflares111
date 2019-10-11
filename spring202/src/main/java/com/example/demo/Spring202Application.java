6172e2730b601d06f41bda03f964e22f9a540058
solarflares111 <56272161+solarflares111@users.noreply.github.com>
2019-10-11 16:52:17 -0400

    Add files via upload

---
+++ spring202/src/main/java/com/example/demo/Spring202Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring202Application.class, args);
+    }
+
+}
---
+++ spring202/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+public class Student {
+    private long id;
+    private String name;
+    private String age;
+    private String height;
+    private int phonenumber;
+    private int year;
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
